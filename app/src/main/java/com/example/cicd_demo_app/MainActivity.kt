a8128dd10d36e0d821c349fea4cf42e7a8fcdfb4
cheetahmail007 <cheetahmail007@gmail.com>
2023-06-08 21:12:49 +0530

    tetsing app

--- app/src/main/java/com/example/cicd_demo_app/MainActivity.kt
+++ app/src/main/java/com/example/cicd_demo_app/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CICD_DEMO_APPTheme {
+
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
